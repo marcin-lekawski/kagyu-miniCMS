2d60f97e20625dfeb1fe27eac182ec06c14703a0
Marcin Łękawski <marcin.lekawski@gmail.com>
2025-06-17 17:16:38 +0200

    prace nad nowym szablonem usuwanie starych menu

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function() {
     // Obsługa kliknięcia ikony burgera
+    // Celujemy bezpośrednio w div.burger z klasą .burger, jak było na początku
     $('.burger-wrap .burger').on('click', function() {
         $(this).toggleClass('open'); // Animacja burgera
         $('.main-menu').toggleClass('open'); // Otwieranie/zamykanie menu bocznego
@@ -12,15 +13,17 @@ $(document).ready(function() {
 
     // Obsługa kliknięcia przycisku zamykania w menu
     $('.main-menu .close-menu').on('click', function() {
-        $('.burger-wrap .burger').removeClass('open'); // Zamknięcie animacji burgera
-        $('.main-menu').removeClass('open'); // Zamykanie menu bocznego
-        $('body').removeClass('overlay-open'); // Ukrywanie overlay'a
+        // Celujemy w div.burger z klasą .burger
+        $('.burger-wrap .burger').removeClass('open'); 
+        $('.main-menu').removeClass('open');
+        $('body').removeClass('overlay-open');
         $('body').removeClass('overlay-full');
         $('body').removeClass('overflow-hidden');
     });
 
-    // Obsługa kliknięcia na overlay (zasłaniającą warstwę)
+    // Obsługa kliknięcia na overlay
     $('.overlay').on('click', function() {
+        // Celujemy w div.burger z klasą .burger
         $('.burger-wrap .burger').removeClass('open');
         $('.main-menu').removeClass('open');
         $('body').removeClass('overlay-open');
@@ -28,26 +31,29 @@ $(document).ready(function() {
         $('body').removeClass('overflow-hidden');
     });
 
-    // Funkcja do obsługi klas 'opac' dla linków w menu głównym
-    // (opac to 'opacity' z dostarczonych stylów)
+    // Funkcja do obsługi klas 'opac' dla linków w menu głównym (bez zmian)
     $('#menu-main li a').on('mouseenter', function() {
         $('#menu-main li a').not(this).addClass('opac');
     }).on('mouseleave', function() {
         $('#menu-main li a').removeClass('opac');
     });
 
-    // Dodatkowa obsługa, aby zamknąć menu po kliknięciu linku (jeśli to potrzebne)
+    // Dodatkowa obsługa, aby zamknąć menu po kliknięciu linku w menu bocznym (bez zmian)
     $('#menu-main li a, .extra-menu-links li a').on('click', function() {
-        // Dodaj małe opóźnienie, aby animacja zamknięcia była widoczna
         setTimeout(function() {
-            $('.burger-wrap .burger').removeClass('open');
+            $('.burger-wrap .burger').removeClass('open'); // Celujemy w div.burger
             $('.main-menu').removeClass('open');
             $('body').removeClass('overlay-open');
             $('body').removeClass('overlay-full');
             $('body').removeClass('overflow-hidden');
-        }, 300); // Czas opóźnienia w milisekundach
+        }, 300);
     });
 
+    // Funkcjonalność przycisku "Powrót na górę"
+    $('#back-to-top').on('click', function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+    });
     // --- Inne skrypty, które możesz mieć w przyszłości ---
-    // np. obsługa mapy, slidera, itp.
 });
